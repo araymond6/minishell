52002b7a8b8e33b6b96b0b56a84a6ebcd0bce58f
Valerie St-Pierre <vst-pier@c3r6p5.42quebec.com>
2023-09-22 13:47:28 -0400

    before pull

--- src/main.c
+++ src/main.c
@@ -6,11 +6,11 @@
 /*   By: vst-pier <vst-pier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/24 13:33:15 by araymond          #+#    #+#             */
-/*   Updated: 2023/09/21 16:58:30 by vst-pier         ###   ########.fr       */
+/*   Updated: 2023/09/21 17:12:03 by vst-pier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-
+/*
 #include "../include/minishell.h"
 
 int main(int argc, char **argv, char **envp)
@@ -22,3 +22,4 @@ int main(int argc, char **argv, char **envp)
 	x_comm(&mini);
 	exit_program(&mini);
 }
+*/
