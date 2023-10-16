67b1190010e20117d6faa5e2b7b3a803ae54751c
Valerie St-Pierre <vst-pier@c1r7p2.42quebec.com>
2023-10-16 12:09:29 -0400

    norminette

--- src/utils.c
+++ src/utils.c
@@ -41,3 +41,26 @@ int	spacentabs_check(t_minishell *mini)
 	}
 	return (1);
 }
+
+int	ft_atoll(const char *str)
+{
+	int			i;
+	int			sign;
+	long long	nb;
+
+	i = 0;
+	sign = 1;
+	nb = 0;
+	if (str[i] == '+' || str[i] == '-')
+	{
+		if (str[i] == '-')
+			sign = -1;
+		i++;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		nb = nb * 10 + (str[i] - 48);
+		i++;
+	}
+	return (sign * nb);
+}
