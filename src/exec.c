1f7d46bc1aa8520d1a522d0b229fda4781eee8ac
Valerie St-Pierre <vst-pier@c1r7p2.42quebec.com>
2023-10-16 08:53:35 -0400

    school

--- src/exec.c
+++ src/exec.c
@@ -1,45 +1,65 @@
 #include "../include/minishell.h"
 
-// check if it<s a build0in or not and execute it
-int	execute_cmd(t_minishell *mini)
+char	*create_cmd(t_minishell *mini, char *cmd)
 {
-	int		exit_code;
-	char	*cmd;
-	char	**tab_path;
-	int		len;
+	cmd = ft_calloc(ft_strlen(mini->s_cmd->path) + 1, sizeof(char));
+	if (cmd == NULL)
+		return (free_scmd(mini->s_cmd), NULL);
+	ft_strlcpy(cmd, mini->s_cmd->path, ft_strlen(mini->s_cmd->path));
+	return (cmd);
+}
+
+char	**create_tab_path(t_minishell *mini, char **tab_path)
+{
+	int	len;
 
 	len = 0;
-	cmd = NULL;
+	while (mini->s_cmd->cmd_arg[len])
+		len++;
+	tab_path = ft_calloc(len + 1, sizeof(char *));
+	if (tab_path == NULL)
+		return (NULL);
+	len = 0;
+	while (mini->s_cmd->cmd_arg[len])
+	{
+		tab_path[len] = ft_calloc(ft_strlen(mini->s_cmd->cmd_arg[len]), sizeof(char));
+		if (tab_path == NULL)
+			return (NULL);
+		ft_strlcpy(tab_path[len], mini->s_cmd->cmd_arg[len], ft_strlen(mini->s_cmd->path));
+		len++;
+	}
+	return (tab_path);
+}
+
+// check if it<s a buildin or not and execute it
+int	execute_cmd(t_minishell *mini)
+{
+	char	*cmd;
+	char	**tab_path;
+
 	if (ft_strlen(mini->s_cmd->cmd) == 0)
 		exit(0);
 	else if (isbuildin(mini->s_cmd->cmd) == 0)
-	{
-		exit_code = execute_buildin(mini);
-		exit(exit_code);
-	}
+		exit(execute_buildin(mini));
 	else
 	{
+		cmd = NULL;
+		tab_path = NULL;
 		signal(SIGQUIT, SIG_DFL);
-		if (execve(mini->s_cmd->path, mini->s_cmd->cmd_arg, NULL) == -1)
-			cmd = ft_calloc(ft_strlen(mini->s_cmd->path) + 1, sizeof(char));
+		cmd = create_cmd(mini, cmd);
 		if (cmd == NULL)
 			return (free_scmd(mini->s_cmd), 1);
-		ft_strlcpy(cmd, mini->s_cmd->path, ft_strlen(mini->s_cmd->path));
-		while (mini->s_cmd->cmd_arg[len])
-			len++;
-		tab_path = ft_calloc(len + 1, sizeof(char*));
+		tab_path = create_tab_path(mini, tab_path);
 		if (tab_path == NULL)
-			return (free(cmd), free_scmd(mini->s_cmd), 1);
-		len = 0;
-		while (mini->s_cmd->cmd_arg[len])
 		{
-			tab_path[len] = ft_calloc(ft_strlen(mini->s_cmd->cmd_arg[len]), sizeof(char));
-			if (tab_path == NULL)
-				return (free(cmd), free_array(tab_path), free_scmd(mini->s_cmd), 1);
-			ft_strlcpy(tab_path[len], mini->s_cmd->cmd_arg[len], ft_strlen(mini->s_cmd->path));
-			len++;
+			free(cmd);
+			free_scmd(mini->s_cmd);
+			if (tab_path)
+				free_array(tab_path);
+			return (1);
 		}
-		ft_strlcpy(cmd, mini->s_cmd->path, ft_strlen(mini->s_cmd->path));
+		write(1, cmd, 100);
+		write(1, "\n", 100);
 		free_scmd(mini->s_cmd);
 		if (execve(cmd, tab_path, mini->envp) == -1)
 		{
